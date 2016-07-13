1c23fbbad25af4e0ed7b19da832ce97ce9f3c14c
Exadra37 <exadra37@gmail.com>
2016-07-13 21:01:29 +0100

    Make first test for method `execute()` green. Related to issue#1.

--- 01-fizz-buzz/src/FizzBuzz.php
+++ 01-fizz-buzz/src/FizzBuzz.php
@@ -2,8 +2,32 @@
 
 class FizzBuzz
 {
-    public function execute($argument1)
+    public function execute($number)
     {
-        // TODO: write logic here
+        $result = '';
+
+        if ($this->isMultipleOfThree($number)) {
+
+            $result = "Fizz";
+        }
+
+        if ($this->isMultipleOfFive($number)) {
+
+            $result .= "Buzz";
+        }
+
+        $result = !empty($result) ? $result : $number;
+
+        return $result;
+    }
+
+    private function isMultipleOfThree($number)
+    {
+        // your turn to write some code
+    }
+
+    private function isMultipleOfFive($number)
+    {
+        // your turn to write some code
     }
 }
